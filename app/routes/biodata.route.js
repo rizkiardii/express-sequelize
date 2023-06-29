589cee1b72ab6a33c3c0cb39165501e5095500a7
rizkiardii <rizkiseptiardi95@gmail.com>
2023-06-29 12:37:35 +0700

    Merubah nama route pada folder routes

--- app/routes/biodata.route.js
+++ app/routes/biodata.route.js
@@ -1,11 +1,10 @@
 const biodataController = require("../controllers/biodata.controller");
 const router = require("express").Router();
 
-router.post("/", biodataController.create);
-router.get("/", biodataController.findAll);
-router.put("/:id", biodataController.update);
-router.put("/:id", biodataController.update);
-router.delete("/:id", biodataController.delete);
-router.get("/:id", biodataController.findOne);
+router.get("/biodata", biodataController.findAll);
+router.post("/biodata/create", biodataController.create);
+router.put("/biodata/update:id", biodataController.update);
+router.delete("/biodata/delete/:id", biodataController.delete);
+router.get("/biodata/:id", biodataController.findOne);
 
 module.exports = router;
